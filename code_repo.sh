81268902205b1e4f36538a77158b93852deb9e57
Joseph Christanto <aetherionz@gmail.com>
2020-01-21 14:42:53 +0700

    code repo

--- code_repo.sh
+++
@@ -1,9 +0,0 @@
-cd D:\dev\documentation\Sinarmas Digital
-
-git init
-git remote add vue https://github.com/josephchrist91/sinarmas-digital-vue
-
-git add .
-git commit -m "code repo"
-
-git push vue master
